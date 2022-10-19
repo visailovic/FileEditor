86d764fc64ff24f160baaa16cf874be84fba8ee5
Velibor Isailovic <velibor.isailovic@gmail.com>
2022-10-19 10:14:59 +0200

    Update FileEditor.java

--- src/main/java/org/example/FileEditor.java
+++ src/main/java/org/example/FileEditor.java
@@ -9,7 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileEditor extends JDialog {
-    private JPanel contentPane;
+    private JPanel contentPane; //Content pane
     private JButton buttonOpen;
     private JButton buttonClose;
     private JButton buttonSave;
